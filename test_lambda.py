8853b4d61fb28bef50d14aa7ddac6dc70b17d6ca
shreyasgp <63000067+shreyasgp@users.noreply.github.com>
2022-05-04 23:22:40 +0530

    Minor Changes

--- test_lambda.py
+++ test_lambda.py
@@ -16,32 +16,35 @@ def lambda_handler(event, context):
     if conn.can_paginate("describe_alarms"):
         paginator = conn.get_paginator('describe_alarms')
         for response in paginator.paginate():
-            for alarm_response in response['MetricAlarms']:
-                alarm_name = alarm_response['AlarmName']
-                alarm_value = alarm_response['StateValue']
-                state_value_digit = states.get(alarm_value)
-                alarm_last_updated_obj = alarm_response['AlarmConfigurationUpdatedTimestamp']
-                last_updated = alarm_updated_obj.strftime('%Y-%m-%d %H:%M:%S %Z')
-                alarm_list.append({alarm_name:(alarm_state_value,last_updated)})
-                # count += 1
-                conn.put_metric_data(
-                    MetricData = [
-                        {
-                            'MetricName':'AlarmState',
-                            'Dimensions':[
-                                {
-                                    'Name':'By Alarm Name',
-                                    'Value':alarm_name
-                                },    
-                            ],
-                            'Timestamp':datetime.datetime.now(),
-                            'Unit':'Count',
-                            'Value':state_value_digit
-                        },    
-                    ],
-                    Namespace='CloudWatchAlarmsNew'
-                )
-
+            if response:
+                for alarm_response in response['MetricAlarms']:
+                    alarm_name = alarm_response['AlarmName']
+                    alarm_value = alarm_response['StateValue']
+                    state_value_digit = states.get(alarm_value)
+                    alarm_last_updated_obj = alarm_response['AlarmConfigurationUpdatedTimestamp']
+                    last_updated = alarm_last_updated_obj.strftime('%Y-%m-%d %H:%M:%S %Z')
+                    alarm_list.append({alarm_name:(alarm_value,last_updated)})
+                    # count += 1
+                    
+                    conn.put_metric_data(
+                        MetricData = [
+                            {
+                                'MetricName':'AlarmState',
+                                'Dimensions':[
+                                    {
+                                        'Name':'By Alarm Name',
+                                        'Value':alarm_name
+                                    },    
+                                ],
+                                'Timestamp':datetime.datetime.now(),
+                                'Unit':'Count',
+                                'Value':state_value_digit
+                            },    
+                        ],
+                        Namespace='CloudWatchAlarmsNew'
+                    )
+            else:
+                _LOGGER.error('Response error')
         # _LOGGER.info({'alarms_count':count})
         for data in alarm_list:
             _LOGGER.info('Alarm_Data:%s',data)
